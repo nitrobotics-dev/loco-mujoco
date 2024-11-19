Cleaned up observations and fixed issue with root quaternion in reward and metrics.
- also updated terminal state handler to use quaternions.

loco_mujoco/core/utils/goals.py
@@ -20,9 +20,6 @@ class Goal(Observation):
         self.visualize_goal = visualize_goal
         super().__init__(obs_name=self.__class__.__name__)
 
-    def _init_from_mj(self, model, data, current_obs_size):
-        raise NotImplementedError
-
     @property
     def has_visual(self):
         raise NotImplementedError
@@ -142,18 +139,12 @@ class GoalTrajArrow(Goal):
     def _init_from_mj(self, model, data, current_obs_size):
         self.min, self.max = [-np.inf] * self.dim, [np.inf] * self.dim
         # todo: This will only work if userdata contains only a single goal and no other info.
-        data_type_ind = [i for i in range(data.userdata.size)]
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + self.dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array([i for i in range(data.userdata.size)])
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + self.dim)])
         self._initialized_from_mj = True
 
     def init_from_traj(self, traj_handler):
-        pass
-        # todo: implement this correctly!
-        # assert trajectories is not None
-        # self._traj_goal_ind = np.concatenate([ind for k, ind in trajectories.keys2ind.items() if k == "VEL_2D"])
-        # self._initialized_from_traj = True
+        self._initialized_from_traj = True
 
     @classmethod
     def get_obs(cls, model, data, ind, backend):
@@ -237,10 +228,8 @@ class GoalTrajMimic(Goal):
 
     def _init_from_mj(self, model, data, current_obs_size):
         self.min, self.max = [-np.inf] * self.dim, [np.inf] * self.dim
-        data_type_ind = [i for i in range(data.userdata.size)]
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + self.dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array([i for i in range(data.userdata.size)])
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + self.dim)])
         for body_name in self._relevant_body_names:
             body_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_BODY, body_name)
             self._relevant_body_ids.append(body_id)

loco_mujoco/core/utils/observations.py
@@ -5,6 +5,8 @@ import numpy as np
 import mujoco
 import jax.numpy as jnp
 
+from loco_mujoco.core.utils.mujoco import mj_jntname2qposid, mj_jntname2qvelid, mj_jntid2qposid, mj_jntid2qvelid
+
 
 def jnt_name2id(name, model):
     """
@@ -92,7 +94,7 @@ class Observation:
         self.name = obs_name
         self.obs_container = None
 
-        # these attributes will be initialized from MjData
+        # these attributes *must* be initialized in the _init_from_mj method
         self.obs_ind = None
         self.data_type_ind = None
         self.min, self.max = None, None
@@ -120,6 +122,12 @@ class Observation:
     def _init_from_mj(self, model, data, current_obs_size):
         """
         Initialize the observation type from the Mujoco data structure and model.
+        This method *must* initialize the following attributes:
+            - obs_ind: Indices of this observation in the observation space.
+            - data_type_ind: Indices of this observation in respective attribute
+                in the Mujoco data structure (like qpos, qvel, xpos, etc.).
+            - min: Minimum values of the observation in the observation space.
+            - max: Maximum values of the observation in the observation space.
 
         Args:
             model: The Mujoco model.
@@ -269,11 +277,8 @@ class BodyPos(SimpleObs):
         dim = len(data.body(self.xml_name).xpos)
         assert dim == self.dim
         self.min, self.max = [-np.inf] * dim, [np.inf] * dim
-        data_type_ind = self.to_list(data.body(self.xml_name).id)
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
-
+        self.data_type_ind = np.array(self.to_list(data.body(self.xml_name).id))
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -295,10 +300,8 @@ class BodyRot(SimpleObs):
         dim = len(data.body(self.xml_name).cvel)
         assert dim == self.dim
         self.min, self.max = [-np.inf] * dim, [np.inf] * dim
-        data_type_ind = self.to_list(data.body(self.xml_name).id)
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array(self.to_list(data.body(self.xml_name).id))
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -320,10 +323,8 @@ class BodyVel(SimpleObs):
         dim = len(data.body(self.xml_name).xquat)
         assert dim == self.dim
         self.min, self.max = [-np.inf] * dim, [np.inf] * dim
-        data_type_ind = self.to_list(data.body(self.xml_name).id)
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array(self.to_list(data.body(self.xml_name).id))
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -344,12 +345,8 @@ class FreeJointPos(SimpleObs):
     def _init_from_mj(self, model, data, current_obs_size):
         # note: free joints do not have limits
         self.min, self.max = [-np.inf] * FreeJointPos.dim, [np.inf] * FreeJointPos.dim
-        free_joint_id = data.joint(self.xml_name).id
-        start_addr = model.jnt_qposadr[free_joint_id]
-        data_type_ind = [i for i in range(start_addr, start_addr + FreeJointPos.dim)]
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + FreeJointPos.dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array(mj_jntname2qposid(self.xml_name, model))
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + FreeJointPos.dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -396,7 +393,6 @@ class FreeJointPosNoXY(FreeJointPos):
 
     def _init_from_mj(self, model, data, current_obs_size):
         super()._init_from_mj(model, data, current_obs_size)
-
         self.min, self.max = self.min[2:], self.max[2:]
         self.data_type_ind = self.data_type_ind[2:]
         self.obs_ind = self.obs_ind[:-2]
@@ -425,11 +421,8 @@ class JointPos(SimpleObs):
             self.min, self.max = [jh.range[0]], [jh.range[1]]
         else:
             self.min, self.max = [-np.inf] * dim, [np.inf] * dim
-        data_type_ind = [model.jnt_qposadr[jh.id]]
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
-
+        self.data_type_ind = np.array(mj_jntid2qposid(jh.id, model))
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -453,12 +446,8 @@ class FreeJointVel(SimpleObs):
         assert dim == self.dim
         # note: free joints do not have limits
         self.min, self.max = [-np.inf] * dim, [np.inf] * dim
-        free_joint_id = data.joint(self.xml_name).id
-        start_addr = model.jnt_dofadr[free_joint_id]
-        data_type_ind = [i for i in range(start_addr, start_addr + FreeJointVel.dim)]
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array(mj_jntname2qvelid(self.xml_name, model))
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -504,10 +493,8 @@ class JointVel(SimpleObs):
         dim = len(data.joint(self.xml_name).qvel)
         assert dim == self.dim
         self.min, self.max = [-np.inf] * dim, [np.inf] * dim
-        data_type_ind = [model.jnt_dofadr[data.joint(self.xml_name).id]]
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array([model.jnt_dofadr[data.joint(self.xml_name).id]])
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -529,10 +516,8 @@ class SitePos(SimpleObs):
         dim = len(data.site(self.xml_name).xpos)
         assert dim == self.dim
         self.min, self.max = [-np.inf] * dim, [np.inf] * dim
-        data_type_ind = self.to_list(data.site(self.xml_name).id)
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array(self.to_list(data.site(self.xml_name).id))
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -556,10 +541,8 @@ class SiteRot(SimpleObs):
         dim = len(data.site(self.xml_name).xmat)
         assert dim == self.dim
         self.min, self.max = [-np.inf] * dim, [np.inf] * dim
-        data_type_ind = self.to_list(data.site(self.xml_name).id)
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array(self.to_list(data.site(self.xml_name).id))
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + dim)])
         self._initialized_from_mj = True
 
     @classmethod
@@ -592,10 +575,8 @@ class Force(Observation):
         self.min, self.max = [-np.inf] * self.dim, [np.inf] * self.dim
         self.data_geom_id1 = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, self.xml_name_geom1)
         self.data_geom_id2 = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, self.xml_name_geom2)
-        data_type_ind = [self.data_geom_id1, self.data_geom_id2]
-        obs_ind = [j for j in range(current_obs_size, current_obs_size + self.dim)]
-        self.data_type_ind = np.array(data_type_ind)
-        self.obs_ind = np.array(obs_ind)
+        self.data_type_ind = np.array([self.data_geom_id1, self.data_geom_id2])
+        self.obs_ind = np.array([j for j in range(current_obs_size, current_obs_size + self.dim)])
         self._initialized_from_mj = True
 
     @classmethod

loco_mujoco/core/utils/terminal_state_handler.py
@@ -1,10 +1,9 @@
 import numpy as np
 import jax.numpy as jnp
-import mujoco
-from jax.scipy.spatial.transform import Rotation as jnp_R
 from scipy.spatial.transform import Rotation as np_R
 
 from loco_mujoco.trajectory import TrajectoryHandler
+from loco_mujoco.core.utils.mujoco import mj_jntname2qposid
 
 
 class TerminalStateHandler:
@@ -16,25 +15,55 @@ class TerminalStateHandler:
 
     def is_absorbing(self, obs, info, data, carry):
         """
-        Check if the current state is terminal.
+        Check if the current state is terminal. Function for CPU Mujoco.
+
+        Args:
+            obs (np.array): shape (n_samples, n_obs), the observations
+            info (dict): the info dictionary
+            data: Mujoco data structure
+            carry: additional carry.
+
+        Returns:
+            Boolean indicating whether the current state is terminal or not.
         """
         raise NotImplementedError
 
     def mjx_is_absorbing(self, obs, info, data, carry):
         """
-        Check if the current state is terminal.
+        Check if the current state is terminal. Function for Mjx.
+
+        Args:
+            obs (np.array): shape (n_samples, n_obs), the observations
+            info (dict): the info dictionary
+            data: Mujoco data structure
+            carry: additional carry.
+
+        Returns:
+            Boolean indicating whether the current state is terminal or not.
         """
         raise NotImplementedError
 
     def _is_absorbing_compat(self, obs, info, data, carry, backend):
         """
-        Check if the current state is terminal.
+        Check if the current state is terminal. Function for CPU Mujoco and Mjx.
+
+        Args:
+            obs (np.array): shape (n_samples, n_obs), the observations
+            info (dict): the info dictionary
+            data: Mujoco data structure
+            carry: additional carry.
+
+        Returns:
+            Boolean indicating whether the current state is terminal or not.
         """
         raise NotImplementedError
 
     def init_from_traj(self, th: TrajectoryHandler):
         """
-        Initialize the TerminalStateHandler from a Trajectory (optional).
+        Initialize the TerminalStateHandler from a Trajectory (Optional).
+
+        Args:
+            th (TrajectoryHandler): The trajectory handler containing the trajectory.
 
         """
         pass
@@ -64,15 +93,13 @@ class TerminalStateHandler:
 
 class HeightBasedTerminalStateHandler(TerminalStateHandler):
     """
-    Check if the current state is terminal based on the height of the pelvis.
+    Check if the current state is terminal based on the height of the root.
     """
     def __init__(self, model, info_props):
         super().__init__(model, info_props)
 
         self.root_height_range = info_props["root_height_healthy_range"]
-        root_free_joint_xml_ind = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, info_props["root_free_joint_xml_name"])
-        assert root_free_joint_xml_ind != -1
-        self.root_free_joint_xml_ind = np.arange(root_free_joint_xml_ind, root_free_joint_xml_ind + 7)
+        self.root_free_joint_xml_ind = np.array(mj_jntname2qposid(info_props["root_free_joint_xml_name"], model))
 
     def is_absorbing(self, obs, info, data, carry):
         return self._is_absorbing_compat(obs, info, data, carry, backend=np)
@@ -90,91 +117,149 @@ class HeightBasedTerminalStateHandler(TerminalStateHandler):
 
 class RootPoseTrajTerminalStateHandler(TerminalStateHandler):
 
-    def __init__(self, model,  info_props, root_height_slack=0.2, root_rot_slack=1.0):
+    def __init__(self, model,  info_props, root_height_margin=0.3, root_rot_margin_degrees=30.0):
         self._initialized = False
 
         self.root_joint_name = info_props["root_free_joint_xml_name"]
 
-        self.root_height_slack = root_height_slack
-        self.root_rot_slack = root_rot_slack
+        self.root_height_margin = root_height_margin
+        self.root_rot_margin_degrees = root_rot_margin_degrees
 
         # to be determined in init_from_traj
         self.root_height_ind = None
         self.root_quat_ind = None
         self.root_height_range = None
-        self.root_rotX_range = None
-        self.root_rotY_range = None
-        self.root_rotZ_range = None
+        self._centroid_quat = None
+        self._valid_threshold = None
 
     def init_from_traj(self, th: TrajectoryHandler):
+        """
+        Initialize the TerminalStateHandler from a Trajectory.
+
+        Args:
+            th (TrajectoryHandler): The trajectory handler containing the trajectory.
+
+        """
         traj = th.traj
         root_ind = traj.info.joint_name2ind_qpos[self.root_joint_name]
         self.root_height_ind = root_ind[2]
         self.root_quat_ind = root_ind[3:7]
         assert len(self.root_quat_ind) == 4
 
-        # convert quaternion to euler angles
-        traj_root_quat = traj.data.qpos[:, self.root_quat_ind]
-        traj_root_euler = np_R.from_quat(traj_root_quat).as_rotvec()
+        # get the root quaternions
+        root_quats = traj.data.qpos[:, self.root_quat_ind]
 
-        # calculate the range of the root pose data
+        # calculate the centroid of the root quaternions and the maximum angular distance from the centroid
+        self._centroid_quat, self._valid_threshold = self._calc_root_rot_centroid_and_margin(root_quats)
+
+        # calculate the range of the root height
         root_height_min = np.min(traj.data.qpos[:, self.root_height_ind])
         root_height_max = np.max(traj.data.qpos[:, self.root_height_ind])
-        root_rot_x_min = np.min(np.fmod(traj_root_euler[:, 0], np.pi))
-        root_rot_x_max = np.max(np.fmod(traj_root_euler[:, 0], np.pi))
-        root_rot_y_min = np.min(np.fmod(traj_root_euler[:, 1], np.pi))
-        root_rot_y_max = np.max(np.fmod(traj_root_euler[:, 1], np.pi))
-        root_rot_z_min = np.min(np.fmod(traj_root_euler[:, 2], np.pi))
-        root_rot_z_max = np.max(np.fmod(traj_root_euler[:, 2], np.pi))
-
-        # calc range and add slack to the range
-        self.root_height_range = (root_height_min-self.root_height_slack, root_height_max+self.root_height_slack)
-        self.root_rotX_range = (root_rot_x_min-self.root_rot_slack, root_rot_x_max+self.root_rot_slack)
-        self.root_rotY_range = (root_rot_y_min-self.root_rot_slack, root_rot_y_max+self.root_rot_slack)
-        self.root_rotZ_range = (root_rot_z_min-self.root_rot_slack, root_rot_z_max+self.root_rot_slack)
+        self.root_height_range = (root_height_min - self.root_height_margin, root_height_max + self.root_height_margin)
 
         self._initialized = True
 
     def is_absorbing(self, obs, info, data, carry):
+        """
+        Check if the current state is terminal. The state is terminal if the root height is outside the range or the
+        root rotation is outside the valid threshold. Function for CPU Mujoco.
+
+        Args:
+            obs (np.array): shape (n_samples, n_obs), the observations
+            info (dict): the info dictionary
+            data: Mujoco data structure
+            carry: additional carry.
+
+        Returns:
+            Boolean indicating whether the current state is terminal or not.
+
+        """
         if self.initialized:
             return self._is_absorbing_compat(obs, info, data, carry, backend=np)
         else:
             return False
 
     def mjx_is_absorbing(self, obs, info, data, carry):
+        """
+        Check if the current state is terminal. The state is terminal if the root height is outside the range or the
+        root rotation is outside the valid threshold. Function for Mjx.
+
+        Args:
+            obs (jnp.array): shape (n_samples, n_obs), the observations
+            info (dict): the info dictionary
+            data: Mjx data structure
+            carry: additional carry.
+
+        Returns:
+            Boolean indicating whether the current state is terminal or not.
+
+        """
         if self.initialized:
             return self._is_absorbing_compat(obs, info, data, carry, backend=jnp)
         else:
             return False
 
     def _is_absorbing_compat(self, obs, info, data, carry, backend):
+        """
+        Check if the current state is terminal. The state is terminal if the root height is outside the range or the
+        root rotation is outside the valid threshold.
 
-        # get rotation backend
-        if backend == np:
-            R = np_R
-        else:
-            R = jnp_R
+        Args:
+            obs (np.array): shape (n_samples, n_obs), the observations
+            info (dict): the info dictionary
+            data: Mujoco data structure
+            carry: additional carry.
+            backend: the backend to use (np or jnp)
 
+        Returns:
+            Boolean indicating whether the current state is terminal or not.
+
+        """
         # get height and rotation of the root joint
         height = data.qpos[self.root_height_ind]
         root_quat = data.qpos[self.root_quat_ind]
-        root_euler = R.from_quat(root_quat).as_rotvec()
-        root_rot_x = backend.fmod(root_euler[0], backend.pi)
-        root_rot_y = backend.fmod(root_euler[1], backend.pi)
-        root_rot_z = backend.fmod(root_euler[2], backend.pi)
 
-        # check if the root pose is outside the range
+        # check if the root height is outside the range
         height_cond = backend.logical_or(backend.less(height, self.root_height_range[0]),
                                          backend.greater(height, self.root_height_range[1]))
-        root_rot_x_cond = backend.logical_or(backend.less(root_rot_x, self.root_rotX_range[0]),
-                                             backend.greater(root_rot_x, self.root_rotX_range[1]))
-        root_rot_y_cond = backend.logical_or(backend.less(root_rot_y, self.root_rotY_range[0]),
-                                             backend.greater(root_rot_y, self.root_rotY_range[1]))
-        root_rot_z_cond = backend.logical_or(backend.less(root_rot_z, self.root_rotZ_range[0]),
-                                             backend.greater(root_rot_z, self.root_rotZ_range[1]))
-        
-        return backend.logical_or(backend.logical_or(height_cond, root_rot_x_cond),
-                                  backend.logical_or(root_rot_y_cond, root_rot_z_cond))
+
+        # check if the root rotation is outside the valid threshold
+        root_quat = root_quat / backend.linalg.norm(root_quat)
+        angular_distance = 2 * backend.arccos(backend.clip(backend.dot(self._centroid_quat, root_quat), -1, 1))
+        root_rot_cond = backend.greater(angular_distance, self._valid_threshold)
+
+        return backend.logical_or(height_cond, root_rot_cond)
+
+    def _calc_root_rot_centroid_and_margin(self, root_quats):
+        """
+        Calculate the centroid of the root quaternions and the maximum angular distance from the centroid.
+
+        Args:
+            root_quats: np.array, shape (n_samples, 4), the root quaternions
+
+        Returns:
+            centroid_quat: np.array, shape (4,), the centroid of the quaternions
+            valid_threshold: float, the maximum angular distance from the centroid
+        """
+
+        # normalize them
+        norm_root_quats = root_quats / np.linalg.norm(root_quats, axis=1, keepdims=True)
+
+        # compute centroid of the quaternions
+        r = np_R.from_quat(norm_root_quats)
+        centroid_quat = r.mean().as_quat()
+
+        # Compute maximum deviation in angular distance
+        dot_products = np.clip(np.einsum('ij,j->i', norm_root_quats,
+                                         centroid_quat), -1, 1)
+        angular_distances = 2 * np.arccos(dot_products)
+
+        max_distance = np.max(angular_distances)
+
+        # Add margin
+        valid_threshold = max_distance + np.radians(self.root_rot_margin_degrees)
+
+        return centroid_quat, valid_threshold
 
     @property
     def initialized(self):

loco_mujoco/utils/metrics.py
@@ -9,6 +9,7 @@ from metriX import DistanceMeasures
 import mujoco
 from loco_mujoco.core.wrappers import SummaryMetrics
 from loco_mujoco.core.utils.math import calc_site_velocities, calculate_relative_site_quatities
+from loco_mujoco.core.utils.mujoco import mj_jntid2qposid, mj_jntid2qvelid, mj_jntname2qposid, mj_jntname2qvelid
 
 
 SUPPORTED_QUANTITIES = ["JointPosition", "JointVelocity", "BodyPosition", "BodyVelocity", "BodyOrientation",
@@ -67,12 +68,15 @@ class MetricsHandler:
 
         model = env.get_model()
         if rel_joint_names is not None:
-            self.rel_joint_ids = [mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, name)
-                                  for name in rel_joint_names if name not in joints_to_ignore]
-            assert -1 not in self.rel_joint_ids, f"Joint {rel_joint_names[self.rel_joint_ids.index(-1)]} not found."
+            self.rel_qpos_ids = [jnp.array(mj_jntid2qposid(name, model)) for name in rel_joint_names
+                                 if name not in joints_to_ignore]
+            self.rel_qvel_ids = [jnp.array(mj_jntid2qvelid(name, model)) for name in rel_joint_names
+                                 if name not in joints_to_ignore]
         else:
-            self.rel_joint_ids = [i for i in range(model.njnt)
-                                  if mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_JOINT, i) not in joints_to_ignore]
+            self.rel_qpos_ids = [jnp.array(mj_jntid2qposid(i, model)) for i in range(model.njnt)
+                                 if mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_JOINT, i) not in joints_to_ignore]
+            self.rel_qvel_ids = [jnp.array(mj_jntid2qvelid(i, model)) for i in range(model.njnt)
+                                 if mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_JOINT, i) not in joints_to_ignore]
 
         if rel_body_names is not None:
             self.rel_body_ids = [mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_BODY, name)
@@ -117,9 +121,14 @@ class MetricsHandler:
                                                            "relative quantities.")
 
         self._vec_calc_site_velocities = jax.vmap(jax.vmap(calc_site_velocities, in_axes=(None, 0, None, None, None, None)), in_axes=(None, 0, None, None, None, None))
-        self._vec_calc_rel_site_quantaties = jax.vmap(jax.vmap(calculate_relative_site_quatities, in_axes=(0, None, None, None, None)), in_axes=(0, None, None, None, None))
+        self._vec_calc_rel_site_quantities = jax.vmap(jax.vmap(calculate_relative_site_quatities, in_axes=(0, None, None, None, None)), in_axes=(0, None, None, None, None))
 
-        self.rel_joint_ids = jnp.array(self.rel_joint_ids)
+        # determine the quaternions in qpos
+        self._quat_in_qpos = jnp.concatenate([jnp.array([False]*3+[True]*4) if len(j) == 7 else jnp.array([False])
+                                              for j in self.rel_qpos_ids])
+        self._not_quat_in_qpos = jnp.invert(self._quat_in_qpos)
+        self.rel_qpos_ids = jnp.concatenate(self.rel_qpos_ids)
+        self.rel_qvel_ids = jnp.concatenate(self.rel_qvel_ids)
         self.rel_body_ids = jnp.array(self.rel_body_ids)
         self.rel_site_ids = jnp.array(self.rel_site_ids)
 
@@ -212,8 +221,20 @@ class MetricsHandler:
 
         # get from trajectory
         traj_qpos = self._traj_data.qpos[self.get_traj_indices(env_states)]
+        # filter for relevant joints
+        qpos, traj_qpos = qpos[..., self.rel_qpos_ids], traj_qpos[..., self.rel_qpos_ids]
 
-        return qpos[..., self.rel_joint_ids], traj_qpos[..., self.rel_joint_ids]
+        # there might be quaternions due to free joints, so we need to convert them
+        # to rotation vector to use metrics in the Euclidean space
+        quat, quat_traj = qpos[..., self._quat_in_qpos], traj_qpos[..., self._quat_in_qpos]
+        quat, quat_traj = quat.reshape(-1, 4), quat_traj.reshape(-1, 4)
+        rot_vec, rot_vec_traj = R.from_quat(quat).as_rotvec(), R.from_quat(quat_traj).as_rotvec()
+        qpos = jnp.concatenate([qpos[..., self._not_quat_in_qpos],
+                                rot_vec.reshape((*qpos.shape[:-1], 3))], axis=-1)
+        traj_qpos = jnp.concatenate([traj_qpos[..., self._not_quat_in_qpos],
+                                     rot_vec_traj.reshape((*traj_qpos.shape[:-1], 3))], axis=-1)
+
+        return qpos, traj_qpos
 
     def get_joint_velocities(self, env_states):
         # get from data
@@ -222,7 +243,7 @@ class MetricsHandler:
         # get from trajectory
         traj_qvel = self._traj_data.qvel[self.get_traj_indices(env_states)]
 
-        return qvel[..., self.rel_joint_ids], traj_qvel[..., self.rel_joint_ids]
+        return qvel[..., self.rel_qvel_ids], traj_qvel[..., self.rel_qvel_ids]
 
     def get_body_positions(self, env_states):
 
@@ -289,13 +310,13 @@ class MetricsHandler:
 
     def get_relative_site_quantities(self, env_states):
 
-        rel_site_pos, rel_site_rotvec, rel_site_vel = self._vec_calc_rel_site_quantaties(
+        rel_site_pos, rel_site_rotvec, rel_site_vel = self._vec_calc_rel_site_quantities(
             env_states.data, self.rel_site_ids, self._site_bodyid[self.rel_site_ids],
             self._body_rootid[self.rel_site_ids], jnp)
 
         traj_states = env_states.additional_carry.traj_state
         traj_data = self._traj_data.get(traj_states.traj_no, traj_states.subtraj_step_no)
-        traj_rel_site_pos, traj_rel_site_rotvec, traj_rel_site_vel = self._vec_calc_rel_site_quantaties(
+        traj_rel_site_pos, traj_rel_site_rotvec, traj_rel_site_vel = self._vec_calc_rel_site_quantities(
             traj_data, self.rel_site_ids, self._site_bodyid[self.rel_site_ids],
             self._body_rootid[self.rel_site_ids], jnp)
 