6c3ad95d3f14e0c53e24fd902612bf4a888c1309
robfiras <f.al-hafez@posteo.de>
2025-01-08 19:04:08 +0100

    small fix in optimize_for_collisions.

--- loco_mujoco/datasets/data_generation/utils.py
+++ loco_mujoco/datasets/data_generation/utils.py
@@ -276,7 +276,7 @@ def optimize_for_collisions(
     sites_for_mimic = env.sites_for_mimic
     target_mocap_bodies = ["target_mocap_body_" + s for s in sites_for_mimic]
     mjspec = add_mocap_bodies(mjspec, sites_for_mimic, target_mocap_bodies)
-    env = LocoEnv.registered_envs[env_name](**robot_conf.env_params, xml_path=mjspec,
+    env = LocoEnv.registered_envs[env_name](**robot_conf.env_params, spec=mjspec,
                                             th_params=dict(random_start=False, fixed_start_conf=(0, 0)))
 
     traj_data, traj_info = interpolate_trajectories(traj.data, traj.info, 1.0 / env.dt)
