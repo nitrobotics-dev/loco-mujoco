0bc567e1f1e3ac5bf07546e42e50e10c529fe9d8
boris-il-forte <boris.ilpossente@hotmail.it>
2025-01-23 18:19:46 +0100

    Starting to work on test handlers
    - implementing first version of trajectory generator
    - working on test state handler

--- tests/test_terminal_state_handler.py
+++ tests/test_terminal_state_handler.py
@@ -13,40 +13,28 @@ from test_conf import *
 DEFAULTS = {"horizon": 1000, "gamma": 0.99, "n_envs":1}
 
 
-@pytest.mark.parametrize("backend", ["jax"])
+@pytest.mark.parametrize("backend", ["jax", "numpy"])
 def test_RootPoseTrajTerminalStateHandler(standing_trajectory, falling_trajectory, backend):
 
     seed = 0
     key = jax.random.PRNGKey(seed)
 
     # define a simple Mjx environment
-    mjx_env = TestHumamoidEnv(enable_mjx=True,
+    mjx_env = TestHumamoidEnv(enable_mjx=backend=="jax",
                               terminal_state_type="RootPoseTrajTerminalStateHandler",
                               **DEFAULTS)
 
     # load trajectory to env
     expert_traj: Trajectory = standing_trajectory
-    backend_type = jnp if backend == "jax" else np
+    nominal_traj: Trajectory = falling_trajectory
+
+    if backend == "numpy":
+        expert_traj.data = expert_traj.data.to_numpy()
+        nominal_traj.data = nominal_traj.data.to_numpy()
+
     mjx_env.load_trajectory(expert_traj)
 
-    # reset the environment in Mjx
-    state = mjx_env.mjx_reset(key)
-    obs_mjx = state.observation
-
-    # "policy" trajectory
-    non_expert_traj = falling_trajectory
-
-    # todo: do what you have to do ...
-
-    # do the same testing for Mujoco function
-    mjx_env.th.to_numpy()
-
-    # reset the environment in Mujoco
-    obs = mjx_env.reset(key)
-
-    # todo: do what you have to do ...
-
-
-
-
-
+    # Create dataset of transitions using the nominal trajectory
+    transitions = mjx_env.generate_trajectory_from_nominal(falling_trajectory)
+    print(len(transitions.absorbings))
+    print(np.argwhere(transitions.absorbings))
