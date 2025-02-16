e20387df4ef5d145b6088e6366d6740804f427d6
robfiras <f.al-hafez@posteo.de>
2025-02-16 21:23:26 +0100

    removed old retarting pipeline.

--- loco_mujoco/datasets/data_generation/__init__.py
+++
@@ -1,2 +0,0 @@
-from .utils import load_robot_conf, load_dataset_conf, ExtendTrajData, optimize_for_collisions, calculate_qvel_with_finite_difference
-from .convert_datasets import convert_single_dataset_of_env, PATH_RAW_DATASET
