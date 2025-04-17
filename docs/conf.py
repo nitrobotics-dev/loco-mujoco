87acc1d75096c532b9ae99f76ea5711968f6b370
robfiras <f.al-hafez@posteo.de>
2025-04-17 23:19:31 +0200

    Version Bump 1.0.0.

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = 'LocoMuJoCo'
 copyright = '2025, Firas Al-Hafez'
 author = 'Firas Al-Hafez'
-release = 'v0.4.1'
+release = 'v1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- loco_mujoco/__init__.py
+++ loco_mujoco/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-__version__ = '0.4.1'
+__version__ = '1.0.0'
 
 
 try:
